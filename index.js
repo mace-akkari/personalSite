9840814ac5e18703f1dcf2fd1c8f718e6410dc5e
Mace <maceplus@gmail.com>
2019-12-18 18:57:46 +0000

    Images & container for carousel

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // Generate image path  
-const imagePaths = Array.from((new Array(3)), (_, i) => `crypto-pic${i}.png`);
+const imagePaths = Array.from((new Array(4)), (_, i) => `crypto-pic${i}.png`);
+
+// Contanier and image elements 
+const container = document.createElement('DIV');
+const imageContainer = document.createElement('OL');
+imagePaths.forEach((path) => {
+    const li = document.createElement('LI');
+    const image = document.createElement('IMG');
+    image.src = path;
+    li.appendChild(image);
+    imageContainer.appendChild(li);
+}); 
+
+container.appendChild(imageContainer);
+document.body.appendChild(container);
 
